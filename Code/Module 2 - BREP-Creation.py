a9eb6ce48bb39ab689e509893fdae5255a5a5ae9
e.fichter <41104642+e3dEF@users.noreply.github.com>
2019-10-25 10:52:37 +0200

    Update Module 2 - BREP-Creation.py

--- Code/Module 2 - BREP-Creation.py
+++ Code/Module 2 - BREP-Creation.py
@@ -1,5 +1,6 @@
 import ifcopenshell.geom
 
+
 def module_2_brep_to_occ(file, guids):
     settings = ifcopenshell.geom.settings()
     settings.set(settings.USE_PYTHON_OPENCASCADE, True)
@@ -12,7 +13,6 @@ def module_2_brep_to_occ(file, guids):
 
 
 def product_to_shape(p, settings):
-
     if p.is_a("IfcOpeningElement") or p.is_a("IfcSite") or p.is_a("IfcAnnotation"):
         return None
     if p.Representation is not None:
@@ -24,26 +24,29 @@ def product_to_shape(p, settings):
     else:
         return None
 
-def visualize_products(guid_to_shape):
-    shapes = []
-    for key in guid_to_shape:
-        shapes.append(guid_to_shape[key])
-    if isinstance(shapes, list):
-        for shape in shapes:
-            ifcopenshell.geom.utils.display_shape(shape)
-    else:
-        ifcopenshell.geom.utils.display_shape(shapes)
 
-
+# create input
 file = ifcopenshell.open("h.ifc")
 guids = []
 for p in file.by_type("IfcWall"):
     guids.append(p.GlobalId)
 
+# create occ shapes from guid list of specific file
 guid_to_shape = module_2_brep_to_occ(file, guids)
+
+# result
 for key in guid_to_shape:
     print(key, guid_to_shape[key])
 
+# def visualize_products(guid_to_shape):
+#     shapes = []
+#     for key in guid_to_shape:
+#         shapes.append(guid_to_shape[key])
+#     if isinstance(shapes, list):
+#         for shape in shapes:
+#             ifcopenshell.geom.utils.display_shape(shape)
+#     else:
+#         ifcopenshell.geom.utils.display_shape(shapes)
 # pyocc_viewer = ifcopenshell.geom.utils.initialize_display()
 # visualize_products(guid_to_shape)
 # pyocc_viewer.FitAll()
